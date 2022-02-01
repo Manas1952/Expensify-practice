830e2134948b30729065cd32691cdc40863ee8b5
Manas1952 <purohitmanas36@gmail.com>
2022-02-01 17:56:26 +0530

    changes

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js',
-      publicPath: '/disto/'
+      publicPath: '/dist/'
     },
     module: {
       rules: [
